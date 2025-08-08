bd3d424557ad1278e24ea7e33c297b580ea3cc16
benmetson <benmetson@gmail.com>
2025-08-08 13:40:02 +0100

    Update supabaseClient.ts

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,7 +1,8 @@
 'use client';
-import { supabase } from '@/lib/supabaseClient';
+
+import { createClient } from '@supabase/supabase-js';
 
 export const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  process.env.NEXT_PUBLIC_SUPABASE_URL as string,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string
 );
