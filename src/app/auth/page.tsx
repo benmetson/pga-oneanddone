f5a5af83d1a5a8e6dbcb9df0cc0bf911fd0d0df6
benmetson <benmetson@gmail.com>
2025-08-08 12:51:54 +0100

    Create page.tsx

---
+++ src/app/auth/page.tsx
@@ -0,0 +1,8 @@
+export default function AuthPage() {
+  return (
+    <main style={{ padding: 20 }}>
+      <h1>Login / Signup</h1>
+      <p>This will be the authentication page.</p>
+    </main>
+  );
+}
